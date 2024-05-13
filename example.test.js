a726b6f9d92027a6aea0432c41207d84b4939bb5
Axel Kaechele <69541858+AxelK1999@users.noreply.github.com>
2024-05-13 14:58:40 -0300

    Add files via upload

--- example.test.js
+++ example.test.js
@@ -1,5 +1,10 @@
-const example = require('./index');
+const {example, inicioSesiónCVUGD} = require('./index');
 
 test('Titulo de busqueda es igual a Google', async() => {
     expect(await example()).toBe("Google");
-});
+},10000);
+
+test('Prueba de inicio de secion con credenciales validos', async()=>{
+    expect(await inicioSesiónCVUGD()).toBe("CAMPUS VIRTUAL UGD");
+},10000000);
+
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { Builder, Browser, By, Key, until } = require('selenium-webdriver')
 async function example() {
   let driver = await new Builder().forBrowser(Browser.EDGE).build()
   try {
-    let result = await driver.get('https://www.google.com/ncr')
+    let result = await driver.get('https://www.google.com')
     //await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN)
     //await driver.wait(until.titleIs('webdriver - Google Search'), 1000)
     
@@ -15,5 +15,47 @@ async function example() {
   }
 }
 
-module.exports = example;
+//Verifica correcto acceso al CVUGD dado las credenciales:
+async function inicioSesiónCVUGD(){
+  let driver = await new Builder().forBrowser(Browser.EDGE).build()
+  try {
+
+    let result = await driver.get('https://campusvirtual.ugd.edu.ar/moodle/login/index.php');
+    await driver.findElement(By.id('username')).sendKeys('67112');
+    await driver.findElement(By.id('password')).sendKeys('axel1530139', Key.RETURN);
+    //key.RETURN -> Al ingresar los datos ejecuta el enter(submit).
+    //await driver.findElement(By.id('password')).click(); 
+
+   //await driver.executeScript("document.querySelector(\"input[type='submit'][value='Ingresar']\").click();")
+    
+    let title = await driver.getTitle();
+    return title;
+
+  } finally {
+    await driver.quit()
+  }
+}
+
+async function nombreCursoCorrectoAlIngresar(){
+  let driver = await new Builder().forBrowser(Browser.EDGE).build()
+  try {
+
+    let result = await driver.get('https://campusvirtual.ugd.edu.ar/moodle/login/index.php');
+    await driver.findElement(By.id('username')).sendKeys('67112');
+    await driver.findElement(By.id('password')).sendKeys('axel1530139', Key.RETURN);
+    
+    await driver.findElement(By.className('coursebox'));
+
+
+    let title = await driver.getTitle();
+    return title;
+
+  } finally {
+    await driver.quit()
+  }
+}
+
+
+
+module.exports = {example, inicioSesiónCVUGD};
 //example();
